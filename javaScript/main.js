76a041b4f1b3711bc794148c304a52fed23680e2
norhs <norhsham02@gmail.com>
2022-10-28 16:08:14 +0200

    landing without nav andstill there is an edit on it

--- javaScript/main.js
+++ javaScript/main.js
@@ -86,3 +86,119 @@ x.addEventListener("click", function() {
     }, 200)
 
 });
+
+
+let landDot1 = document.querySelector(".landing .dots #dot1")
+let landDot2 = document.querySelector(".landing .dots #dot2")
+let landDot3 = document.querySelector(".landing .dots #dot3")
+let landDot4 = document.querySelector(".landing .dots #dot4")
+let landimg1 = document.querySelector(".landing .images .mainimage")
+let landimg2 = document.querySelector(".landing .images .image1")
+let landimg3 = document.querySelector(".landing .images .image2")
+let landimg4 = document.querySelector(".landing .images .image3")
+
+function imagenum1() {
+    landDot1.classList.add("active")
+    landDot2.classList.remove("active")
+    landDot3.classList.remove("active")
+    landDot4.classList.remove("active")
+    landimg1.style.left = "0"
+    landimg2.style.left = "100%"
+    landimg3.style.left = "200%"
+    landimg4.style.left = "300%"
+}
+
+function imagenum2() {
+    landDot1.classList.remove("active")
+    landDot2.classList.add("active")
+    landDot3.classList.remove("active")
+    landDot4.classList.remove("active")
+    landimg1.style.left = "-100%"
+    landimg2.style.left = "0%"
+    landimg3.style.left = "100%"
+    landimg4.style.left = "200%"
+}
+
+function imagenum3() {
+    landDot1.classList.remove("active")
+    landDot2.classList.remove("active")
+    landDot4.classList.remove("active")
+    landDot3.classList.add("active")
+    landimg1.style.left = "-200%"
+    landimg2.style.left = "-100%"
+    landimg3.style.left = "0%"
+    landimg4.style.left = "100%"
+}
+function imagenum4() {
+    landDot1.classList.remove("active")
+    landDot2.classList.remove("active")
+    landDot3.classList.remove("active")
+    landDot4.classList.add("active")
+
+    landimg1.style.left = "-300%"
+    landimg2.style.left = "-200%"
+    landimg3.style.left = "-100%"
+    landimg4.style.left = "0%"
+}
+landDot1.addEventListener("click", function imagenum1() {
+    landDot1.classList.add("active")
+    landDot2.classList.remove("active")
+    landDot3.classList.remove("active")
+    landDot4.classList.remove("active")
+    landimg1.style.left = "0"
+    landimg2.style.left = "100%"
+    landimg3.style.left = "200%"
+    landimg4.style.left = "300%"
+})
+
+
+landDot2.addEventListener("click", function imagenum2() {
+    landDot1.classList.remove("active")
+    landDot2.classList.add("active")
+    landDot3.classList.remove("active")
+    landDot4.classList.remove("active")
+    landimg1.style.left = "-100%"
+    landimg2.style.left = "0%"
+    landimg3.style.left = "100%"
+    landimg4.style.left = "200%"
+})
+
+landDot3.addEventListener("click", function imagenum3() {
+    landDot1.classList.remove("active")
+    landDot2.classList.remove("active")
+    landDot4.classList.remove("active")
+    landDot3.classList.add("active")
+    landimg1.style.left = "-200%"
+    landimg2.style.left = "-100%"
+    landimg3.style.left = "0%"
+    landimg4.style.left = "100%"
+})
+landDot4.addEventListener("click", function imagenum4() {
+    landDot1.classList.remove("active")
+    landDot2.classList.remove("active")
+    landDot3.classList.remove("active")
+    landDot4.classList.add("active")
+    landimg1.style.left = "-300%"
+    landimg2.style.left = "-200%"
+    landimg3.style.left = "-100%"
+    landimg4.style.left = "0%"
+})
+
+function all() {
+    setTimeout(imagenum1, 16000)
+    setTimeout(imagenum2, 4000)
+    setTimeout(imagenum3, 8000)
+    setTimeout(imagenum4, 12000)
+}
+all();
+setInterval(all, 17000)
+let imagesLanding = document.getElementById("imagesLanding")
+
+window.onresize = WindowSize;
+window.onload = WindowSize;
+function WindowSize() {
+    let myWidth = window.innerWidth;
+    imagesLanding.style.width = `${myWidth.toString()}px - 20px`
+        // الرقم اللي تحت ده هو ال النسبه بين الطول والعرض للصوره علشان اخلي الاب يتغير علي اساس الصوره 
+    imagesLanding.style.height = `calc(${myWidth.toString() / 1.81}px)`
+}
