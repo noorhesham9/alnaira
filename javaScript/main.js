d7426b388209905c3b5a31e96cd4a69d37792f2c
norhs <norhsham02@gmail.com>
2022-11-14 04:57:53 +0200

    finish footer

--- javaScript/main.js
+++ javaScript/main.js
@@ -52,7 +52,11 @@ window.onscroll = function () {
     OrangelineINHead.style.setProperty("--opacity", opacityNumber());
   }
   if (window.scrollY >= 260) {
-    totopbutton.style.top = "calc(100% - 115px)";
+    if (window.innerWidth < 420) {
+      totopbutton.style.top = "calc(100% - 70px)";
+    } else {
+      totopbutton.style.top = "calc(100% - 115px)";
+    }
   } else {
     totopbutton.style.top = "calc(-200px)";
   }
@@ -613,3 +617,145 @@ leftforlinks.addEventListener("click", function () {
   leftforlinks.style.display = "none";
   rightforlinks.style.display = "flex";
 });
+
+// canvas
+let resizeReset = function () {
+  w = canvasBody.width = window.innerWidth;
+  h = canvasBody.height = window.innerHeight;
+};
+
+const opts = {
+  particleColor: "rgb(108,108,108)",
+  lineColor: "rgb(108,108,108)",
+  particleAmount: 30,
+  defaultSpeed: 1,
+  variantSpeed: 1,
+  defaultRadius: 2,
+  variantRadius: 2,
+  linkRadius: 200,
+};
+
+window.addEventListener("resize", function () {
+  deBouncer();
+});
+
+let deBouncer = function () {
+  clearTimeout(tid);
+  tid = setTimeout(function () {
+    resizeReset();
+  }, delay);
+};
+
+let checkDistance = function (x1, y1, x2, y2) {
+  return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+};
+
+let linkPoints = function (point1, hubs) {
+  for (let i = 0; i < hubs.length; i++) {
+    let distance = checkDistance(point1.x, point1.y, hubs[i].x, hubs[i].y);
+    let opacity = 1 - distance / opts.linkRadius;
+    if (opacity > 0) {
+      drawArea.lineWidth = 0.5;
+      drawArea.strokeStyle = `rgba(${rgb[0]}, ${rgb[1]}, ${rgb[2]}, ${opacity})`;
+      drawArea.beginPath();
+      drawArea.moveTo(point1.x, point1.y);
+      drawArea.lineTo(hubs[i].x, hubs[i].y);
+      drawArea.closePath();
+      drawArea.stroke();
+    }
+  }
+};
+
+Particle = function (xPos, yPos) {
+  this.x = Math.random() * w;
+  this.y = Math.random() * h;
+  this.speed = opts.defaultSpeed + Math.random() * opts.variantSpeed;
+  this.directionAngle = Math.floor(Math.random() * 360);
+  this.color = opts.particleColor;
+  this.radius = opts.defaultRadius + Math.random() * opts.variantRadius;
+  this.vector = {
+    x: Math.cos(this.directionAngle) * this.speed,
+    y: Math.sin(this.directionAngle) * this.speed,
+  };
+  this.update = function () {
+    this.border();
+    this.x += this.vector.x;
+    this.y += this.vector.y;
+  };
+  this.border = function () {
+    if (this.x >= w || this.x <= 0) {
+      this.vector.x *= -1;
+    }
+    if (this.y >= h || this.y <= 0) {
+      this.vector.y *= -1;
+    }
+    if (this.x > w) this.x = w;
+    if (this.y > h) this.y = h;
+    if (this.x < 0) this.x = 0;
+    if (this.y < 0) this.y = 0;
+  };
+  this.draw = function () {
+    drawArea.beginPath();
+    drawArea.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    drawArea.closePath();
+    drawArea.fillStyle = this.color;
+    drawArea.fill();
+  };
+};
+
+function setup() {
+  particles = [];
+  resizeReset();
+  for (let i = 0; i < opts.particleAmount; i++) {
+    particles.push(new Particle());
+  }
+  window.requestAnimationFrame(loop);
+}
+
+function loop() {
+  window.requestAnimationFrame(loop);
+  drawArea.clearRect(0, 0, w, h);
+  for (let i = 0; i < particles.length; i++) {
+    particles[i].update();
+    particles[i].draw();
+  }
+  for (let i = 0; i < particles.length; i++) {
+    linkPoints(particles[i], particles);
+  }
+}
+
+const canvasBody = document.getElementById("canvas"),
+  drawArea = canvasBody.getContext("2d");
+let delay = 200,
+  tid,
+  rgb = opts.lineColor.match(/\d+/g);
+resizeReset();
+setup();
+
+// try div
+let sky, center;
+
+function dot(i) {
+  const size = Math.round(Math.random() + 1);
+  const root = document.createElement("span");
+  root.style.top = center.y + "px";
+  root.style.left = center.x + "px";
+  root.classList.add("star", `size-${size}`, `axis-${i}`);
+  return root;
+}
+
+function clear() {
+  sky.innerHTML = "";
+}
+
+function init() {
+  sky = document.querySelector("#sky");
+  center = {
+    x: sky.clientWidth / 2,
+    y: sky.clientHeight / 2,
+  };
+  clear();
+  for (let i = 0; i < 360; i++) sky.appendChild(dot(i));
+}
+
+window.onload = init;
